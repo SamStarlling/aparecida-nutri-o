0faac89c6d37b4eefc0d12871f7aa060665f35d8
SamStarlling <samelabarbosa.06@gmail.com>
2021-05-29 00:03:08 -0300

    Mudança do título da página através da manipulação do DOM no JS

---
+++ js/principal.js
@@ -0,0 +1,4 @@
+let tituloPrincipal = document.querySelector(".titulos"); //Seleciona a 1th elemento com a class titulo
+
+tituloPrincipal.textContent = "Aparecida Nutricionista";
+console.log(tituloPrincipal);

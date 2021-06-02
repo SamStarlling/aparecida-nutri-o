fd889bf5c06b2b2c84ee5de2eb788cb1223b1ed1
SamStarlling <samelabarbosa.06@gmail.com>
2021-06-02 02:06:17 -0300

    adicionado o loop for para o cálculo do IMC de todos os items da tabela

--- js/principal.js
+++ js/principal.js
@@ -1,26 +1,30 @@
 let tituloPrincipal = document.querySelector(".titulos"); //Seleciona a 1th elemento com a class titulo
 tituloPrincipal.textContent = "Aparecida Nutricionista";
 
-let primeiroPaciente = document.querySelector("#primeiro-paciente");
+let pacientes = document.querySelectorAll(".paciente");
 
-let tdPeso = primeiroPaciente.querySelector(".info-peso");
-let peso = tdPeso.textContent;
+//Loop para calcular o imc de todos os itens da tabela
+for (let i = 0; i < pacientes.length; i++) {
+  let paciente = pacientes[i];
+  let tdPeso = paciente.querySelector(".info-peso");
+  let peso = tdPeso.textContent;
 
-let tdAltura = primeiroPaciente.querySelector(".info-altura");
-let altura = tdAltura.textContent;
+  let tdAltura = paciente.querySelector(".info-altura");
+  let altura = tdAltura.textContent;
 
-let tdImc = primeiroPaciente.querySelector(".info-imc");
+  let tdImc = paciente.querySelector(".info-imc");
 
-let pesoEhValido = true;
-let alturaEhValida = true;
+  let pesoEhValido = true;
+  let alturaEhValida = true;
 
-if (peso <= 0 || peso >= 1000) {
-  pesoEhValido = false;
-  tdImc.textContent = "Peso é inválido!";
-} else if (altura <= 0 || altura >= 3.0) {
-  alturaEhValida = false;
-  tdImc.textContent = "Altura é inválida!";
-} else {
-  let imc = peso / Math.pow(altura, 2);
-  tdImc.textContent = imc;
+  if (peso <= 0 || peso >= 1000) {
+    pesoEhValido = false;
+    tdImc.textContent = "Peso é inválido!";
+  } else if (altura <= 0 || altura >= 3.0) {
+    alturaEhValida = false;
+    tdImc.textContent = "Altura é inválida!";
+  } else {
+    let imc = peso / Math.pow(altura, 2);
+    tdImc.textContent = imc;
+  }
 }
